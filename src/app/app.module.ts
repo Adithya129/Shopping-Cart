fea6951200be6313db2d559a83a7d4a5dfc67992
adithya <adithyas124@gmail.com>
2021-05-04 17:43:38 +0530

    home commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Router,RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
+const router:Routes=[
+  {path:'home',component:HomeComponent},
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    RouterModule.forRoot(router)
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
